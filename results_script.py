5ee03f6641510ed0e7f720fc77645f5e88b0e24c
Medha <medhapotluri@gmail.com>
2020-05-10 23:38:38 -0400

    jer results only variance

--- results_script.py
+++ results_script.py
@@ -10,7 +10,7 @@ pp = pprint.PrettyPrinter()
 pprint = pp.pprint
   
 # csv file name 
-file_name = "vids 5-6/50658"
+file_name = "jeremy 5-7/50704"
 ROW_LIMIT = None # set to none if want all
   
 # initializing the titles and rows list 
